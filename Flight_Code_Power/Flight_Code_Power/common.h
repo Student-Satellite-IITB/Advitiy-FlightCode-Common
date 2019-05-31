845e4cc911af41a4fa9707646cf3549362687d0d
PRASHANTKURREY <prashantkurrey61520@gmail.com>
2019-05-31 20:23:41 +0530

    Started Flight Code for Power. WIP: Added some new constants

--- Flight_Code_Power/Flight_Code_Power/common.h
+++ Flight_Code_Power/Flight_Code_Power/common.h
@@ -18,9 +18,20 @@
 #define nom_idle_mode 0x88
 #define low_power_mode 0x93
 #define extreme_low_power_mode 0xA3
-// #define emergency_mode 0x9X  //to be decided
+#define emergency_mode 0x91  //to be decided
 #define restart_mode 0xB2
 #define kill_mode 0xBE
-
+//end of operational mode
+//constants
+#define BAUDRATE 9600
+#define v_4v 0x0D9A
+#define v_3E5v 0x0C00 
+#define v_3E3v 0x0B5C
+#define v_threshold_2 0x0A66 
+#define KILL 0x0A		   // Defined randomly
+#define RESTART 0xA0	   // Defined randomly
+#define w_threshold 0x12   // Defined randomly
+#define EEPROM_ADDRESS 0xA0
+#define TWI_BAUDSETTING TWI_BAUD(F_CPU, BAUDRATE)
 
 #endif 
