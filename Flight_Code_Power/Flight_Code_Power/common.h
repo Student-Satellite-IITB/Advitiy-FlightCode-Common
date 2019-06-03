4b2a89b4a89bedca922f05321cd79308f543d435
PRASHANTKURREY <prashantkurrey61520@gmail.com>
2019-06-03 20:52:02 +0530

    Started Flight Code for Power. WIP: added function for sleep

--- Flight_Code_Power/Flight_Code_Power/common.h
+++ Flight_Code_Power/Flight_Code_Power/common.h
@@ -9,29 +9,33 @@
 #define COMMON_H
 #include "avr_compiler.h"
 // operational mode
-#define preflight_mode 0X4C
-#define launch_mode 0x70
-#define detumb_pre_mode 0x85
-#define detumb_post_mode 0x86
-#define nom_transmit_mode 0x89
-#define nom_uplink_mode 0x8A
-#define nom_idle_mode 0x88
-#define low_power_mode 0x93
-#define extreme_low_power_mode 0xA3
-#define emergency_mode 0x91  //to be decided
-#define restart_mode 0xB2
-#define kill_mode 0xBE
+#define PREFLIGHT_MODE 0X4C
+#define LAUNCH_MODE 0x70
+#define DETUMB_PRE_MODE 0x85
+#define DETUMB_POST_MODE 0x86
+#define NOM_TRANSMIT_MODE 0x89
+#define NOM_UPLINK_MODE 0x8A
+#define NOM_IDLE_MODE 0x88
+#define LOW_POWER_MODE 0x93
+#define EXTREME_LOW_POWER_MODE 0xA3
+#define EMERGENCY_MODE 0x91  //to be decided
+#define RESTART_MODE 0xB2
+#define KILL_MODE 0xBE
 //end of operational mode
 //constants
 #define BAUDRATE 9600
-#define v_4v 0x0D9A
-#define v_3E5v 0x0C00 
-#define v_3E3v 0x0B5C
-#define v_threshold_2 0x0A66 
+#define V_4V 0x0D9A
+#define V_3E5V 0x0C00 
+#define V_3E3V 0x0B5C
+#define V_THRESHOLD_2 0x0A66 
 #define KILL 0x0A		   // Defined randomly
 #define RESTART 0xA0	   // Defined randomly
-#define w_threshold 0x12   // Defined randomly
+#define W_THRESHOLD 0x12   // Defined randomly
 #define EEPROM_ADDRESS 0xA0
 #define TWI_BAUDSETTING TWI_BAUD(F_CPU, BAUDRATE)
+
+void POWER_SAVE_ENABLE();
+void POWER_SAVE_DISABLE();
+void EXTERNAL_INT_PINDO();
 
 #endif 
